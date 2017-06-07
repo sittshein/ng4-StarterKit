a0b4a2077fb1329cc63005050625b7a0013a5778
John Samuel Smith <John Samuel Smith>
2017-06-07 22:24:05 +0800

    create CoreModule and add simple navigation

---
+++ src/app/core/nav/nav.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+
+class MenuItem {
+  constructor(public caption: string, public link: any[]) { }
+}
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css'],
+})
+export class NavComponent implements OnInit {
+  title = 'Angular 4 StarterKit';
+  menuItems: MenuItem[];
+
+  ngOnInit() {
+    this.menuItems = [
+      { caption: 'Dashboard', link: ['/dashboard'] },
+      { caption: 'Characters', link: ['/characters'] },
+      { caption: 'Vehicles', link: ['/vehicles'] },
+      { caption: 'Admin', link: ['/admin'] },
+      { caption: 'Login', link: ['/login'] },
+    ];
+  }
+
+  constructor() { }
+
+}
